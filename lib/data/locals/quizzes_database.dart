fb6d976e38390456473abaa8de09d9ea7bb8e1e2
thicmn <thichu08@gmail.com>
2025-11-20 17:40:08 +0700

    create quizzer  database

--- lib/data/locals/quizzes_database.dart
+++ lib/data/locals/quizzes_database.dart
@@ -4,38 +4,24 @@ import 'package:drivevn/data/models/answer_collection.dart';
 import 'package:drivevn/data/models/question_collection.dart';
 import 'package:flutter/services.dart';
 import 'package:isar_community/isar.dart';
-import 'package:path_provider/path_provider.dart';
 
 class QuizzesDatabase {
-  static Isar? _isar;
+  final Isar isar;
 
-  static Future<void> initialize() async {
-    if (_isar != null) return;
+  QuizzesDatabase({required this.isar});
 
-    final dir = await getApplicationDocumentsDirectory();
-    _isar = await Isar.open(
-      [QuestionCollectionSchema, AnswerCollectionSchema],
-      directory: dir.path,
-      name: 'quizzesDB',
-    );
-
-    await _seedDatabase();
-  }
-
-  static Isar get isar {
-    if (_isar == null) {
-      throw Exception('Isar database has not been initialized.');
-    }
-    return _isar!;
-  }
-
-  static Future<void> _seedDatabase() async {
+  /// hàm để import dữ liệu ban đầu vào database
+  /// Chỉ chạy 1 lần khi database rỗng (lần đầu cài app)
+  Future<void> importDatabase() async {
+    // Đếm số lượng câu hỏi đã có trong database
     final questionCount = await isar.questionCollections.count();
+    // Nếu đã có dữ liệu rồi thì return (tránh import lại)
     if (questionCount > 0) {
       // Database already seeded
       return;
     }
 
+    // Đọc JSON từ assets
     final jsonString = await rootBundle.loadString(
       'assets/jsons/questions.json',
     );
@@ -45,8 +31,10 @@ class QuizzesDatabase {
     final answers = <AnswerCollection>[];
 
     for (var questionJson in jsonList) {
+      // Tạo object QuestionCollection từ JSON
       final question =
           QuestionCollection()
+            // Toán tử cascade (..) cho phép gọi nhiều setter liên tiếp
             ..number = questionJson['number']
             ..question = questionJson['question']
             ..category = questionJson['category']
@@ -54,22 +42,43 @@ class QuizzesDatabase {
             ..explanation = questionJson['explanation'];
 
       for (var answerJson in questionJson['answers']) {
+        // Tạo object AnswerCollection từ JSON
         final answer =
             AnswerCollection()
               ..text = answerJson['text']
               ..correct = answerJson['correct'];
+        // Thêm answer vào IsarLinks của question (tạo quan hệ 1-nhiều)
+        // question.answers là IsarLinks<AnswerCollection>
         question.answers.add(answer);
         answers.add(answer);
       }
       questions.add(question);
     }
 
+    // writeTxn() tạo một transaction để ghi dữ liệu
+    // Transaction đảm bảo: tất cả thành công hoặc tất cả thất bại (atomicity)
     await isar.writeTxn(() async {
-      await isar.answerCollections.putAll(answers);
-      await isar.questionCollections.putAll(questions);
+      await isar.answerCollections.putAll(
+        answers,
+      ); // Insert answers (chưa có link)
+      await isar.questionCollections.putAll(
+        questions,
+      ); // Insert question (chưa có link)
       for (var question in questions) {
-        await question.answers.save();
+        // save() lưu các links (quan hệ) đã add ở trên
+        // Điều này tạo liên kết trong database giữa question và answers của nó
+        await question.answers.save(); // Tạo link (BÂY GIỜ MỚI CÓ QUAN HỆ)
+        /** Điều này làm: **
+         * - Tạo bảng trung gian (junction table) trong Isar
+         * - Lưu mapping: `question_id ↔ answer_ids`
+         **Bảng ẩn `question_answer_links` (do Isar tự tạo):**
+         */
       }
+      // Phải insert answers TRƯỚC, vì:
+      // - answers cần có ID
+      // - question.answers.save() cần answer IDs để tạo link
+      // đảm bảo Integrity (Tính toàn vẹn dữ liệu)
+      // tăng Performance
     });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,39 @@
+import 'package:drivevn/core/constants/get_it.dart';
 import 'package:drivevn/core/router/app_router.dart';
+import 'package:drivevn/data/locals/quizzes_database.dart';
+import 'package:drivevn/data/models/answer_collection.dart';
+import 'package:drivevn/data/models/question_collection.dart';
 import 'package:flutter/material.dart';
+import 'package:isar_community/isar.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final prefs = await SharedPreferences.getInstance();
+  getIt.registerSingleton<SharedPreferences>(prefs);
+  await initializeDB();
   runApp(const MyApp());
+}
+
+/// Phương thức khởi tạo database
+/// Chỉ chạy 1 lần khi app khởi động
+Future<void> initializeDB() async {
+  // final Isar? isar = Isar.getInstance();
+  // if (isar != null) return;
+
+  // Mở Isar database
+  final dir = await getApplicationDocumentsDirectory();
+  final isar = await Isar.open(
+    // Danh sách các schema (cấu trúc bảng) cần sử dụng
+    [QuestionCollectionSchema, AnswerCollectionSchema],
+    directory: dir.path,
+    name: 'quizzesDB', // Tên file database (sẽ tạo file quizzesDB.isar)
+  );
+  getIt.registerSingleton<Isar>(isar);
+
+  await QuizzesDatabase(isar: isar).importDatabase();
 }
 
 class MyApp extends StatelessWidget {
@@ -15,9 +46,7 @@ class MyApp extends StatelessWidget {
       title: 'drivevn',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.amber,
-        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
         useMaterial3: true,
       ),
       routerConfig: AppRouter().appRouter,
